ec35e8b4da0600fe270aea9fde3126b87b9e911f
furkanbesirli <frknnnbesirli@gmail.com>
2022-04-25 02:08:41 +0300

    C# Kamp 3. Gün Kodları

---
+++ Collections/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> isimler = new List<string> { "Furkan", "Sena", "Eren"};
+            Console.WriteLine(isimler[0]);
+            Console.WriteLine(isimler[1]);
+            Console.WriteLine(isimler[2]);
+            isimler.Add("Sude");
+            Console.WriteLine(isimler[3]);
+            Console.WriteLine(isimler[1]);
+
+        }
+    }
+}
---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> names = new MyList<string>();
+            names.Add("Burukali");
+
+
+
+        }
+    }
+}
